b9911dd5da2d638bdd96cb77a6fa5ec697afd5f2
Maxim <max@mail.ru>
2024-03-11 12:26:24 +0200

    fgg

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pyttsx4
-import pyaudio
+
 import speech_recognition as sr
 for index, name in enumerate(sr.Microphone.list_microphone_names()):
     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
 engige = pyttsx4.init()
 engige.say('Привет, Макс!')
-engige.runAndWait()
+engige.runAndWait(" Hi ")
